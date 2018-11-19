42c305520b7d4c1e423dd760b14bc8b4574d89d9
Dreft12 <jorgeelieceraldana.92@gmail.com>
2018-11-18 23:53:39 -0500

    Agregando forms
    Se agregaron los formularios de ingreso y registro

--- app/src/main/java/com/example/eapal/findmyparking/MapsActivity.java
+++ app/src/main/java/com/example/eapal/findmyparking/MapsActivity.java
@@ -1,86 +1,122 @@
 package com.example.eapal.findmyparking;
 
-import android.os.CountDownTimer;
-import android.support.annotation.NonNull;
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private DatabaseReference databaseReference;
-    private ArrayList<Marker> tmpRealTimeMarker = new ArrayList<>();
-    private ArrayList<Marker> realTimeMarkers = new ArrayList<>();
+    private Marker marker;
+    double lat = 0.0;
+    double lng = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
 
-        databaseReference = FirebaseDatabase.getInstance().getReference();
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+
+        if (status == ConnectionResult.SUCCESS) {
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        } else {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, (Activity) getApplicationContext(), 10);
+            dialog.show();
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        miUbicacion();
+        /*mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        UiSettings uiSettings = mMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);*/
 
-        databaseReference.child("usuarios").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+        // Add a marker in Sydney and move the camera
+        /*LatLng colombia = new LatLng(10.903236, -74.800793);
+        mMap.addMarker(new MarkerOptions().position(colombia).title("Desde las compuectas de mi casa...").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        float zoomLevel = 16;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(colombia, zoomLevel));*/
+    }
 
-                for (Marker marker: realTimeMarkers){
-                    marker.remove();
-                }
+    private void agregarMarcador(double lat, double lng) {
+        LatLng coords = new LatLng(lat, lng);
+        CameraUpdate miUbicacion = CameraUpdateFactory.newLatLngZoom(coords, 16);
 
-                for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+        if (marker != null) {
+            marker.remove();
+        }
 
-                    MapMarkers mm = snapshot.getValue(MapMarkers.class);
-                    Double lat = mm.getLat();
-                    Double lgn = mm.getLgn();
-                    MarkerOptions markerOptions = new MarkerOptions();
-                    markerOptions.position(new LatLng(lat, lgn));
+        marker = mMap.addMarker(new MarkerOptions()
+                .position(coords)
+                .title("Ubicación Actual")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        mMap.animateCamera(miUbicacion);
+    }
 
-                    tmpRealTimeMarker.add(mMap.addMarker(markerOptions));
-                }
+    private void actualizarUbicacion(Location location) {
+        if (location != null) {
+            lat = location.getLatitude();
+            lng = location.getLongitude();
+            agregarMarcador(lat, lng);
+        }
+    }
 
-                realTimeMarkers.clear();
-                realTimeMarkers.addAll(tmpRealTimeMarker);
-            }
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            actualizarUbicacion(location);
+        }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
 
-            }
-        });
+        }
 
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private void miUbicacion() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        actualizarUbicacion(location);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 15000, 0, locationListener);
     }
 }
