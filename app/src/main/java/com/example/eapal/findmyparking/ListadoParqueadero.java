b486ebcf9df06a52788c3e7abc38e33b5b48ceeb
Dreft12 <jorgeelieceraldana.92@gmail.com>
2018-11-20 00:32:15 -0500

    Agregando Recycler a nuevo Activity

---
+++ app/src/main/java/com/example/eapal/findmyparking/ListadoParqueadero.java
@@ -0,0 +1,13 @@
+package com.example.eapal.findmyparking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListadoParqueadero extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_parqueadero);
+    }
+}
