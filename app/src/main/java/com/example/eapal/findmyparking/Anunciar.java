2bd0866f8cdf55470e4c63e30d8e16ceda549561
Enrique Pallares <eapallaress@gmail.com>
2018-11-22 11:20:16 -0500

    Se agregaron validaciones

--- app/src/main/java/com/example/eapal/findmyparking/Anunciar.java
+++ app/src/main/java/com/example/eapal/findmyparking/Anunciar.java
@@ -2,12 +2,60 @@ package com.example.eapal.findmyparking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Anunciar extends AppCompatActivity {
+
+    private EditText txtNombreParqueadero, txtTelefonoParqueadero, txtPrecioParqueadero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anunciar);
+
+        txtNombreParqueadero = findViewById(R.id.txtParqueo);
+        txtTelefonoParqueadero = findViewById(R.id.txtTelePar);
+        txtPrecioParqueadero = findViewById(R.id.txtPrecio);
+    }
+
+    public boolean validar() {
+        if (txtNombreParqueadero.getText().toString().isEmpty()) {
+            txtNombreParqueadero.setError(getResources().getString(R.string.error_field_required));
+            txtNombreParqueadero.requestFocus();
+
+            return false;
+        }
+        if (txtTelefonoParqueadero.getText().toString().isEmpty()) {
+            txtTelefonoParqueadero.setError(getResources().getString(R.string.error_field_required));
+            txtTelefonoParqueadero.requestFocus();
+
+            return false;
+        }
+        if (Integer.parseInt(txtTelefonoParqueadero.getText().toString()) == 0) {
+            txtTelefonoParqueadero.setError(getResources().getString(R.string.error_cero));
+            txtTelefonoParqueadero.requestFocus();
+
+            return false;
+        }
+        if (txtTelefonoParqueadero.getText().length() < 6 || txtTelefonoParqueadero.getText().length() > 15) {
+            txtTelefonoParqueadero.setError(getResources().getString(R.string.error_longitud));
+            txtTelefonoParqueadero.requestFocus();
+
+            return false;
+        }
+        if (txtPrecioParqueadero.getText().toString().isEmpty()) {
+            txtPrecioParqueadero.setError(getResources().getString(R.string.error_field_required));
+            txtPrecioParqueadero.requestFocus();
+
+            return false;
+        }
+        if (Integer.parseInt(txtPrecioParqueadero.getText().toString()) == 0) {
+            txtPrecioParqueadero.setError(getResources().getString(R.string.error_cero));
+            txtPrecioParqueadero.requestFocus();
+
+            return false;
+        }
+
+        return true;
     }
 }
